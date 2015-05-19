fe34bf073bd6ee3946098d0f4cafe3ad9459eba0
Julien Watelet <julienw5@hotmail.com>
2015-05-19 11:29:28 +0200

    no message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <head>
-    <title>La newsletter de MonSite.fr</title>
+    <title>La newsletter de Aeria-app</title>
 </head>
 <body>
  <?php
